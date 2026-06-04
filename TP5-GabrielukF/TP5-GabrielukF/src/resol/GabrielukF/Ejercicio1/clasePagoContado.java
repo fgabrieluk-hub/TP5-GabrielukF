6af75561961cc6b30d7fcb8e4329023c52b54c55
fgabrieluk-hub <fgabrieluk@icop.edu.ar>
2026-06-04 18:22:33 -0300

    Delete TP5-GabrielukF/TP5-GabrielukF/src/resol/GabrielukF/Ejercicio1/clasePagoContado.java

--- TP5-GabrielukF/TP5-GabrielukF/src/resol/GabrielukF/Ejercicio1/clasePagoContado.java
+++
@@ -1,9 +0,0 @@
-package resol.GabrielukF.Ejercicio1;
-
-public class clasePagoContado implements clasePago {
-
-    @Override
-    public double calcularPrecio(double precioBase) {
-        return precioBase * 0.85;
-    }
-}
